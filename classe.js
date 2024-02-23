fc442b57b0bb4b9d906b45a3219af9af77f34e8e
LUIZ EDUARDO COSTA <luiz_costa4@estudante.sesisenai.org.br>
2024-02-23 16:42:10 -0300

    Versão Teste Esfera

--- classe.js
+++ classe.js
@@ -1,1 +1,15 @@
 // class volume
+
+class Esferav{
+    constructor(raioesfera, volumeesfera){
+        this.raioesfera = raioesfera
+        this.volumeesfera = volumeesfera
+
+        this.volumeesfera = (4/3) * (3.14) * (raioesfera * raioesfera * raioesfera)
+    }
+
+calcularvolumeEsfera(){
+    respostaesfera = "O volume da sua esfera é de: "+ volumeesfera +"cm³."
+
+}
+}
--- principal.js
+++ principal.js
@@ -1,1 +1,15 @@
 // variáveis e addeventlistener
+
+let respostaesfera = document.getElementById("respostaesfera")
+let calcularesfera = document.getElementById("calcularesfera")
+
+calcularesfera.addEventListener("click", ()=>{
+
+let raioesfera = Number(document.getElementById("raioesfera").value)    
+let volumeesfera = 0
+
+const esferaa = new Esferav(raioesfera, volumeesfera)
+
+let resultadoEsfera = esferaa.calcularvolumeEsfera()
+respostaesfera.innerHTML += (resultadoEsfera)
+})
